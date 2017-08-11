b6fd5095d4154d25acdcd5a552847cb2dd67ee32
Rajat Parajuli <073bct532.rajat@pcampus.edu.np>
2017-08-11 21:40:45 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import sys
+
+
+if(len(sys.argv)<2):
+	print ("Usage python3 main.py filename")
+	exit()	
+
 import cv2
 import numpy as np
 import math
@@ -7,24 +14,14 @@ from PIL import Image
 import engine
 import m_engine
 
-from scipy.spatial import Delaunay
 
-def slope(pt1,pt2):
-    x1,y1 = pt1
-    y1,y2 = pt2
-    return ((y2-y1)/(x2-x1))
 
-def dist(pt1,pt2):
-    x1,y1 = pt1
-    x2,y2 = pt
-    return ((((x2-x1)**2) + ((y2-y1)**2))**(1/2))
-
-rect = cv2.imread("rect.png")
-drawing = cv2.imread("drawing.png")
-test = cv2.imread("new-test.png")
+test = cv2.imread(sys.argv[1])
 
 img = test
 
+cv2.imshow("Supplied Image",img)
+cv2.waitKey(1)
 #img = cv2.resize(test,(200,200))
 
 
@@ -37,10 +34,6 @@ img_gray = cv2.cvtColor(img_inv,cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(img_gray,127,255, 0)
 im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-#print (hierarchy)
-
-#print (len(contours))
-
 view_rects = []
 views = []
 contours_new = []
@@ -52,14 +45,8 @@ for i in range(2):
 
     x,y,w,h = cv2.boundingRect(cnt)
 
-    #print (cnt,"\n")
 
-    #print ((x,y),(w,h),"\n")
-
-   
     view = copy.deepcopy(img[y:y+h,x:x+w])
-
-    #view = cv2.resize(view,(200,200))
 
     contour_new = [cnt-(x,y)]
 
@@ -71,6 +58,7 @@ for i in range(2):
     view_rects.append((x,y,w,h))
 
     cv2.drawContours(img, [cnt], 0, (0,255,0), 1)
+
 
 views_T = []
 
@@ -110,6 +98,7 @@ l_v = np.array(views_proc[0])
 f_v = np.array(views_proc[1])
 
 
+
 h1,l1 =  l_v.shape[:2]
 
 h2,l2 =  f_v.shape[:2]
@@ -128,6 +117,7 @@ for i in range(l2):
 layer = []
 
 #object building algorithm
+print("\n\nBuilding Object...\n\n")
 for i in range(l1):
     for j in range(h1):
         if   l_v[j][i] == 0:
@@ -149,7 +139,7 @@ l,b,h = temp.shape[:3]
 new_ind = []
 big = 0
 
-print("\n\nGenerating Surface...\n\n")
+print("\n\nExtracting surface of the object...\n\n")
 t1 = time.time()
 #surface finding algorithm
 for i,j,k in ind:
@@ -193,29 +183,6 @@ print("Time Elapsed = ",time.time()-t1)
 
 
 
-m_engine.main(new_ind_np)
-
-
-'''
-fo = open("obj",'wb')
-
-np.save(fo,new_ind_np)
-'''
-
-
-#engine.renderer(new_ind_np,-l/2,-b/2,-3*h)
-
-
-
-'''
-cv2.drawContours(views[0], contours_new[0], 0, (0,255,0), 1)
-cv2.drawContours(views[1], contours_new[1], 0, (0,255,0), 1)
-'''
-
-#cv2.imshow("1",thresh)
-#cv2.imshow("2",img)
-#cv2.imshow("3",views[0])
-#cv2.imshow("4",views[1])
-#cv2.waitKey(0)
+m_engine.main(new_ind_np,len(obj_3d),len(obj_3d[0]),len(obj_3d[0][0]))
 
 
